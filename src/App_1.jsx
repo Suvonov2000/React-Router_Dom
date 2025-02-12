1620313381c720a6428f0ee1d9bdd518e71efc33
Suvonov2000 <suvonov200@gmail.com>
2025-02-12 21:00:01 +0900

    revision

--- src/App_1.jsx
+++ src/App_1.jsx
@@ -1,30 +1,47 @@
 import React, { Component } from "react";
 
+const wrapper = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  margin: "auto",
+  width: "100%",
+  height: "100vh",
+  flexDirection: "column",
+};
+
 class App_1 extends Component {
   constructor() {
     super();
 
     this.state = {
-      count: 0,
+      name: "",
+      surname: "",
     };
   }
   render() {
-    const increment = () => {
-      this.setState({
-        count: this.state.count + 1,
-      });
-    };
-    const decrement = () => {
-      if (this.state.count > 0)
-        this.setState({
-          count: this.state.count - 1,
-        });
-    };
     return (
       <div>
-        <button onClick={increment}>+</button>
-        {this.state.count}
-        <button onClick={decrement}>-</button>
+        <input
+          onChange={(e) => {
+            this.setState({
+              name: e.target.value,
+            });
+          }}
+          type="text"
+          placeholder="Enter your name"
+        />
+        <input
+          onChange={(e) => {
+            this.setState({
+              surname: e.target.value,
+            });
+          }}
+          type="text"
+          placeholder="Enter your surname"
+        />
+        <h1>{this.state.name}</h1>
+        <h1>{this.state.surname}</h1>
       </div>
     );
   }
